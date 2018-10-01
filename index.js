f29a8a5c3a9b024dfee202bc833a243f061551d9
Russ yev <ruslanyev@gmail.com>
2018-10-01 10:54:53 -0700

    open target blank for the yelp review

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ function populateMap(data, map) {
                 secondaryColor: '#333333',
                 size: 'sm',
             },
-        }).bindPopup(`${business.name} <br>Rating: ${business.rating}/5 <br>Reviews: ${business.review_count} <br><a aria-label='Read more about ${business.name} on Yelp' href=${business.url}>Yelp</a>`).openPopup().addTo(map);
+        }).bindPopup(`${business.name} <br>Rating: ${business.rating}/5 <br>Reviews: ${business.review_count} <br><a target="_blank" aria-label='Read more about ${business.name} on Yelp' href=${business.url}>Yelp</a>`).openPopup().addTo(map);
     });
     watchHomeButton();  
 };
